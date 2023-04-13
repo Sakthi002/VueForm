d9cae4324077cb86aaedfd507f9bdd33f905a32e
Sakthi002 <sakthivel.munusamy@ladybirdweb.com>
2023-04-13 11:02:42 +0530

    files added

---
+++ vueform.config.js
@@ -0,0 +1,9 @@
+import en from '@vueform/vueform/locales/en'
+import vueform from '@vueform/vueform/themes/vueform'
+
+export default {
+  theme: vueform,
+  locales: { en },
+  locale: 'en',
+  apiKey: 'yeya-j8m3-6euz-3cxu-xtyh',
+}
